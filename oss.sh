75f1404a3b59c2a1e8ed96f9a7d28f604ca678f0
gustmdtj <99joe@naver.com>
2024-05-01 23:29:22 +0900

    add file exist exception

--- oss.sh
+++ oss.sh
@@ -1,4 +1,11 @@
-#! /bin/bash 
+#! /bin/bash
+
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
+then
+	echo "usage: ./oss.sh file1 file2 file3"
+	exit
+fi  
+ 
 echo "************OSS1 - Project1************"
 echo "*        StudentID : 12182891         *"
 echo "*        Name : HyeonSeung Seo        *"
