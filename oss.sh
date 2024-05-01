3ef2949de3e45ffb2949123f092bd6b5816fd414
gustmdtj <99joe@naver.com>
2024-05-01 22:57:35 +0900

    add student info

---
+++ oss.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+echo "************OSS1 - Project1************"
+echo "*        StudentID : 12182891         *"
+echo "*        Name : HyeonSeung Seo        *"
+echo "***************************************"
+
